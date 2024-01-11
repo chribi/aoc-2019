67ee88b5fe417b58110914b9de8d49f5e6f4835c
Christoph Bier <christoph.bier@gmx.de>
2024-01-11 18:14:08 +0100

    Rewrite day 2 using new IntVM

--- Aoc02/Program.cs
+++ Aoc02/Program.cs
@@ -1,59 +1,38 @@
 using LibAoc;
 using static LibAoc.LogUtils;
 using static LibAoc.Test;
+using IntCode;
 
 int SolvePart1(IEnumerable<string> input) {
-    var program = ReadProgram(input.First());
-    return Run(program, 12, 2);
+    var vm = new IntVM(input.First());
+    return Run(vm, 12, 2);
 }
 
 int SolvePart2(IEnumerable<string> input) {
-    var program = ReadProgram(input.First());
+    var vm = new IntVM(input.First());
     var target = 19690720;
     for (var sum = 1; ; sum++) {
         for (var noun = 0; noun <= sum; noun++) {
             var verb = sum - noun;
-            if (Run(program, noun, verb) == target)
+            if (Run(vm, noun, verb) == target)
                 return 100 * noun + verb;
         }
     }
 }
 
-int[] ReadProgram(string input) {
-    return Input.GetIntNumbers(input).ToArray();
-}
-
-int Run(int[] program, int noun, int verb) {
-    // Create copy
-    program = program.ToArray();
-    program[1] = noun;
-    program[2] = verb;
-    RunProgram(program);
-    return program[0];
-}
-
-void RunProgram(int[] program) {
-    var ip = 0;
-    while (program[ip] != 99) {
-        var ap = program[ip + 1];
-        var bp = program[ip + 2];
-        var cp = program[ip + 3];
-
-        var a = program[ap];
-        var b = program[bp];
-        var result = program[ip] == 1 ? a + b : a * b;
-
-        program[cp] = result;
-
-        ip += 4;
-    }
+int Run(IntVM vm, int noun, int verb) {
+    vm.Reset();
+    vm.Memory[1] = noun;
+    vm.Memory[2] = verb;
+    vm.Run();
+    return vm.Memory[0];
 }
 
 if (args.Length == 0) {
     EnableLogging = true;
-    var program = ReadProgram("1,9,10,3,2,3,11,0,99,30,40,50");
-    RunProgram(program);
-    AssertEqual(program[0], 3500);
+    var program = new IntVM("1,9,10,3,2,3,11,0,99,30,40,50");
+    program.Run();
+    AssertEqual(program.Memory[0], 3500);
 } else {
     Utils.AocMain(args, SolvePart1);
     Utils.AocMain(args, SolvePart2);
